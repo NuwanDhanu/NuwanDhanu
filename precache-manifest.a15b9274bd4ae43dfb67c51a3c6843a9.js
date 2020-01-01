07f4695047697f6dc110afe0497b5f57e4061356
nuwandhanu <nuwandhanushka93@gmail.com>
2020-01-01 23:47:45 +0800

    Updates

---
+++ precache-manifest.a15b9274bd4ae43dfb67c51a3c6843a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d448e81200c3f693d98a847cf2d9d4",
+    "url": "/nuwandhanu/index.html"
+  },
+  {
+    "revision": "703e7b73f3a9c8d862cf",
+    "url": "/nuwandhanu/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0de437b4bdc48e13a6a8",
+    "url": "/nuwandhanu/static/js/2.f42be21d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/nuwandhanu/static/js/2.f42be21d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "703e7b73f3a9c8d862cf",
+    "url": "/nuwandhanu/static/js/main.e4b575d6.chunk.js"
+  },
+  {
+    "revision": "c32eab4e5506c1baf6ab",
+    "url": "/nuwandhanu/static/js/runtime-main.b2ff03ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nuwandhanu/static/media/logo.5d5d9eef.svg"
+  }
+]);
