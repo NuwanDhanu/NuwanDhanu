94a1db7b6ecff59be0ce11fb525389b7f7cc12c0
nuwandhanu <nuwandhanushka93@gmail.com>
2020-01-04 23:51:51 +0800

    Adding  a dynamic element

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
+        <span>{this.formatCout()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatCout() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
